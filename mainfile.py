a4364dfbfa14915a22337b5597e1758ed890bb9b
mathieuhalpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-02-15 15:03:46 -0700

    Add files via upload

---
+++ mainfile.py
@@ -0,0 +1,25 @@
+import random
+ngram = {}
+corpus = "The quick brown fox jumped over the lazy dog."
+corpus.lower().replace('"','').replace("'",'').replace('\n','').replace(')','').replace('(','').replace('[','').replace(']','').replace('’','').replace("“",'').replace("”",'')
+     
+for sentence in corpus.split('.'):
+    for i in range(1, len(sentence.split(' '))):
+        word_pair = (sentence.split(' ')[i - 2], sentence.split(' ')[i - 1])
+        if '' in word_pair:
+            continue
+        if (word_pair) not in ngram:
+            ngram[word_pair] = []
+        ngram[word_pair].append(sentence.split(' ')[i])
+    
+word_pair = random.choice(list(ngram.keys())) 
+out = word_pair[0] + ' ' + word_pair[1] + ' '
+
+while True:
+    if word_pair not in ngram.keys():
+        break  
+    third = random.choice(list(ngram[word_pair]))
+    out += third + ' '
+    word_pair = (word_pair[1], third)
+    
+print ('output banter: \n', out)
