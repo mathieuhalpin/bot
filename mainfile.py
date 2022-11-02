6032f3c69bfa816748b09ac2bdecd0d1b5d6b355
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-11-01 19:56:47 -0500

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -8,27 +8,28 @@ f.close()
 
 
 
-import random
-ngram = {}
-corpus = corpus.lower().replace('"','').replace("'",'').replace('\n','').replace(')','').replace('(','').replace('[','').replace(']','').replace('’','').replace("“",'').replace("”",'')
+function main:
+    import random
+    ngram = {}
+    corpus = corpus.lower().replace('"','').replace("'",'').replace('\n','').replace(')','').replace('(','').replace('[','').replace(']','').replace('’','').replace("“",'').replace("”",'')
      
-for sentence in corpus.split('.'):
-    for i in range(1, len(sentence.split(' '))):
-        word_pair = (sentence.split(' ')[i - 2], sentence.split(' ')[i - 1])
-        if '' in word_pair:
-            continue
-        if (word_pair) not in ngram:
-            ngram[word_pair] = []
-        ngram[word_pair].append(sentence.split(' ')[i])
+        for sentence in corpus.split('.'):
+            for i in range(1, len(sentence.split(' '))):
+                word_pair = (sentence.split(' ')[i - 2], sentence.split(' ')[i - 1])
+            if '' in word_pair:
+                continue
+            if (word_pair) not in ngram:
+             ngram[word_pair] = []
+             ngram[word_pair].append(sentence.split(' ')[i])
     
-word_pair = random.choice(list(ngram.keys())) 
-out = word_pair[0] + ' ' + word_pair[1] + ' '
+    word_pair = random.choice(list(ngram.keys())) 
+    out = word_pair[0] + ' ' + word_pair[1] + ' '
 
-while True:
-    if word_pair not in ngram.keys():
-        break  
-    third = random.choice(list(ngram[word_pair]))
-    out += third + ' '
-    word_pair = (word_pair[1], third)
+    while True:
+        if word_pair not in ngram.keys():
+            break  
+        third = random.choice(list(ngram[word_pair]))
+        out += third + ' '
+        word_pair = (word_pair[1], third)
     
-print ('output banter: \n', out)
+    print ('output banter: \n', out)
